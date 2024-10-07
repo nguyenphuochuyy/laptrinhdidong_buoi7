6fdd6a551197c06953bff653c845feb9beecb810
nguyenphuochuyy <phuochuy10023@gmail.com>
2024-10-07 18:52:19 +0700

    add to do

--- Lab7a/App.js
+++ Lab7a/App.js
@@ -1,20 +1,100 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState , useEffect} from "react"
+import { Button, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+
+
+const Item = ({items})=>{
+  return(
+    <View>
+        <View style={styles.todoItem}>
+        <Text>ID Công Việc : {items.id} </Text>
+        <Text>Công Việc : {items.name} </Text>
+        </View>
+       
+    </View>
+  )
+}
 export default function App() {
+
+  const handle = async () => {
+    var todo = {
+
+      name : "công việc 200"
+    }
+   const response = await fetch('https://645906864eb3f674df8478c5.mockapi.io/api/v1/todolist', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(todo)
+  })
+
+  const newTodo = await response.json()
+  setData(preData => [...preData , newTodo]);
+  
+}
+
+  const [data , setData] = useState([]);
+  useEffect(()=>{
+    fetch('https://645906864eb3f674df8478c5.mockapi.io/api/v1/todolist')
+    .then(res=>res.json())
+    .then(data => {
+      setData(data)
+    })
+  },[])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <View style = {styles.title}>
+        <Text>TO DO LIST</Text>
+       </View>
+       <View style={styles.layout}>
+  
+          <Button title = 'Thêm' onPress = {handle}/>
+          <Button title = 'Sửa' ></Button>
+          <Button  title = 'Xóa'></Button>
+
+       </View>
+
+      <View style = {styles.flashlist}> 
+      <FlatList 
+        data={data}
+        renderItem={({item}) => <Item items = {item} />}
+        keyExtractor={item => item.id}
+      />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginVertical : 50,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  title : {
+    alignItems : 'center'
+  },
+  layout : {
+    marginTop : 50,
+    flexDirection : 'row',
+    justifyContent : 'space-around',
+  
+  },
+  flashlist : {
+    marginTop : 100,
+    flex : 2,
+    alignItems : 'center'
+  },
+  todoItem:{
+    borderWidth : 1 ,
+    borderColor : '#ddd',
+    width : 300,
+    marginHorizontal : 10,
+    marginVertical : 10,
+    alignItems : 'center',
+    padding : 5
+  }
+
 });
